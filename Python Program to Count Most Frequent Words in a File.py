47b9a35231bbf5cc3e67aee6e9d515b8d24d89a1
Subin-Vidhu <85066028+Subin-Vidhu@users.noreply.github.com>
2022-07-01 13:58:13 +0530

    Created Python Program to Count Most Frequent Words in a File

---
+++ Python Program to Count Most Frequent Words in a File.py
@@ -0,0 +1,9 @@
+words = []
+with open("file.txt", "r") as f:
+    for line in f:
+        words.extend(line.split())
+
+from collections import Counter
+counts = Counter(words)
+top5 = counts.most_common(5)
+print(top5)
