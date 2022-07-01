9f6fc7f132ccaf18c95ce2678b4f2f03dd854d48
Subin-Vidhu <85066028+Subin-Vidhu@users.noreply.github.com>
2022-07-01 14:00:15 +0530

    Created Password Authentication using Python

---
+++ Password Authentication using Python.py
@@ -0,0 +1,10 @@
+import getpass
+database = {"aman.kharwal": "123456", "kharwal.aman": "654321"}
+username = input("Enter Your Username : ")
+password = getpass.getpass("Enter Your Password : ")
+for i in database.keys():
+    if username == i:
+        while password != database.get(i):
+            password = getpass.getpass("Enter Your Password Again : ")
+        break
+print("Verified")
